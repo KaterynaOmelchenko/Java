5e9622b13e86beb507003f7a8c6c7b1a30203afd
KaterynaOmelchenko <kateomel11@gmail.com>
2020-03-07 23:51:08 -0800

    new assertion

---
+++ JUnit5/src/test/java/assertions/Assertions5new.java
@@ -0,0 +1,14 @@
+package assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Assertions5new
+{
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+}
