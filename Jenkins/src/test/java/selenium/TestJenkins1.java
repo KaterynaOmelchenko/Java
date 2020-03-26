9ce65e1b8581fe9cc62add425292231559acd07d
KaterynaOmelchenko <kateomel11@gmail.com>
2020-03-25 21:37:11 -0700

    jenkins

---
+++ Jenkins/src/test/java/selenium/TestJenkins1.java
@@ -0,0 +1,46 @@
+package selenium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class TestJenkins1
+
+{ private WebDriver driver;
+
+	@BeforeAll
+	static void setUpBeforeClass()
+	{
+		WebDriverManager.chromedriver().setup();
+	}
+	
+	@BeforeEach
+	void setUp()
+	{
+		driver = new ChromeDriver();
+		
+	}
+	@Test
+	void test()
+	{
+		System.out.println("Hi School!");
+		driver.get("https://pasv.us/en");
+		String welcome = driver.findElement(By.cssSelector(".t480__title.t-title.t-title_xs.t-margin_auto"))
+				.getText();
+		assertEquals("We teach programming\nand QA", welcome );  
+	}
+	
+	@AfterEach
+	void tearDown()
+	{
+		driver.quit();
+	}
+}
