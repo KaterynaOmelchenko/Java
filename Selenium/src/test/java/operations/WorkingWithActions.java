5c3131e492f62d11fa2f3c954a6d6e5f8162b680
KaterynaOmelchenko <kateomel11@gmail.com>
2020-04-07 21:50:44 -0700

    working with actions

--- Selenium/src/test/java/operations/WorkingWithActions.java
+++ Selenium/src/test/java/operations/WorkingWithActions.java
@@ -9,8 +9,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -31,19 +33,64 @@ private Actions builder;
 	void setUp()
 	{
 		driver = new ChromeDriver();
+		builder = new Actions(driver);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get("http://book.theautomatedtester.co.uk/chapter4/");
 	}
+	
 	@Test
-	void test()
+	void moveMouseWithAlert()
 	{
+		driver.get("http://book.theautomatedtester.co.uk/chapter4");
+		WebElement mouseOver = driver.findElement(By.id("hoverOver"));
+		builder.moveToElement(mouseOver).perform();
+		Alert alert = driver.switchTo().alert();
+		alert.accept();
+	}
+	
+	@Test
+	void moveMouseByOffset()
+	{
+		driver.get("http://webminal.org");
+		WebElement button = driver.findElement(By.linkText("Register"));
+		builder.moveByOffset(button.getLocation().getX() + 6 , button.getLocation().getY() + 6)
+			.click().perform();
+		assertEquals("Join", driver.findElement(By.xpath("//h2")).getText());
+	}
+	
+	@Test
+	void dragAndDrop()
+	{
+		driver.get("https://demoqa.com/droppable/");
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
+		builder.dragAndDrop(source, target).perform();;
+		assertEquals("Dropped!", target.getText());
+	}
+	
+	@Test
+	void dragByOffset()
+	{
+		driver.get("https://demoqa.com/droppable/");
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
+		
+		int xSource = source.getLocation().getX();
+		int ySource = source.getLocation().getY();
+		
+		int xTarget = target.getLocation().getX();
+		int yTarget = target.getLocation().getY();
+		
+		int xMove = xTarget - xSource + 10;
+		int yMove = yTarget - ySource + 10;
+		
+		builder.dragAndDropBy(source, xMove, yMove).perform();
 		
 	}
 	
 	@AfterEach
 	void tearDown()
 	{
-		driver.quit();
+		//driver.quit();
 	}
 }
